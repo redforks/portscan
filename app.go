cca69fe3b6ffa7a6049d68d85e73507927c29a27
Red Forks <redforks@gmail.com>
2015-10-31 12:11:02 +0800

    Reformat code use golang standard format

--- app.go
+++ app.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "net"
-  "time"
-  "sync"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+	"time"
 )
 
 var (
-  host string // The host address to scan
+	host string // The host address to scan
 )
 
 func init() {
-  if len(os.Args) != 2 {
-    fmt.Fprintf(os.Stderr, "Usage: %s host\n", os.Args[0])
-    os.Exit(1)
-  }
-  host = os.Args[1]
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s host\n", os.Args[0])
+		os.Exit(1)
+	}
+	host = os.Args[1]
 }
 
 func main() {
-  d := net.Dialer{Timeout: 10 * time.Second}
-  p := make(chan bool, 500) // make 500 parallel connection
-  wg := sync.WaitGroup{}
+	d := net.Dialer{Timeout: 10 * time.Second}
+	p := make(chan bool, 500) // make 500 parallel connection
+	wg := sync.WaitGroup{}
 
-  c := func(port int) {
-    conn, err := d.Dial(`tcp`, fmt.Sprintf(`%s:%d`, host, port))
-    if err == nil {
-      conn.Close()
-      fmt.Printf("%d passed\n", port)
-    }
-    <-p
-    wg.Done()
-  }
+	c := func(port int) {
+		conn, err := d.Dial(`tcp`, fmt.Sprintf(`%s:%d`, host, port))
+		if err == nil {
+			conn.Close()
+			fmt.Printf("%d passed\n", port)
+		}
+		<-p
+		wg.Done()
+	}
 
-  wg.Add(65536)
-  for i:=0; i < 65536; i++ {
-    p<-true
-    go c(i)
-  }
+	wg.Add(65536)
+	for i := 0; i < 65536; i++ {
+		p <- true
+		go c(i)
+	}
 
-  wg.Wait()
+	wg.Wait()
 }
